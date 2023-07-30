11a13ff7b5730756f7d8ac0fd0b542c05f9e1d05
veerababug39 <veerababug39@gmail.com>
2023-07-30 17:34:42 +0000

    Even Odd App Solution

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onIncrease = () => {
+    const randomNumber = this.getRandomNumber()
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const displayText = count % 2 === 0 ? 'Even' : 'Odd'
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <h1 className="main-heading">Count {count}</h1>
+          <p className="type-heading">Count is {displayText}</p>
+          <br />
+          <button type="button" onClick={this.onIncrease} className="button">
+            Increment
+          </button>
+          <p>*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
